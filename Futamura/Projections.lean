c3249e81c2e8e3a2e9ab70fd3dd3935b363668ab
James Lee-Jones <james@lee-jones.co.uk>
2026-04-14 17:29:12 +0100

    refactor: bundle correctness proofs
    Include correctness proofs with interp, compiler and mix.

--- Futamura/Projections.lean
+++ Futamura/Projections.lean
@@ -1,23 +1,33 @@
 /--
 A representation of a programming language.
 
-Progams in language `L` that take inputs of type `i` and return outputs of type `o` have type `L.Prog i o`.
+Progams in language `L` that take inputs of type `I` and return outputs of type `O` have type `L.Prog I O`.
 Each language has an associated evaluator, so that program `p` can be run on input `x` with `L.eval p x`.
 -/
 structure Lang where
   /-- The type of programs. -/
   Prog: Type → Type → Type
   /-- Evaluates a program on an input. -/
-  eval: {i o : Type} → Prog i o → i → o
+  eval: {I O : Type} → Prog I O → I → O
 
 /-- An interpreter for `S` written in `T`: a `T`-program that takes a source program and its input, and produces the output. -/
-def interp {i o : Type} (S T : Lang) :=
-  T.Prog (S.Prog i o × i) o
+structure Interp {I O : Type} (S T : Lang) where
+  /-- The interpreter program. -/
+  prog: T.Prog (S.Prog I O × I) O
+  /-- Running the interpreter yields the same result as evaluating the source program directly. -/
+  correct: ∀ (p : S.Prog I O) (x: I), S.eval p x = T.eval prog (p, x)
 
-/-- A compiler from `S` to `T` written in `T`: a `T`-program that takes a source program from `i` to `o` and returns a `T` program from `i` to `o`. -/
-def compiler {i o : Type} (S T : Lang) :=
-  T.Prog (S.Prog i o) (T.Prog i o)
+/-- A compiler from `S` to `T` written in `T`: a `T`-program that takes a source program from `I` to `O` and returns a `T` program from `I` to `O`. -/
+structure Compiler {I O : Type} (S T : Lang) where
+  /-- The compiler program.-/
+  prog: T.Prog (S.Prog I O) (T.Prog I O)
+  /-- Compiling and running a program yileds the same result as evaluating the source program directly. -/
+  correct: ∀ (p : S.Prog I O) (x : I), S.eval p x = T.eval (T.eval prog p) x
 
 /-- A specializer (partial evaluator) for `T`: a `T`-program that takes a program expecting a static-dynamic pair and a static input, and produces a residual program over just the dynamic input. -/
-def mix {s d o : Type} (T : Lang) :=
-  T.Prog (T.Prog (s × d) o × s) (T.Prog d o)
+structure Mix {S D O : Type} (T : Lang) where
+  /-- The partial evaluator program. -/
+  prog: T.Prog ((T.Prog (S × D) O) × S) (T.Prog D O)
+  /-- The residual program is equivalent to the original with the static input fixed. -/
+  correct: ∀ (p : T.Prog (S × D) O) (s : S) (x : D),
+    T.eval p (s, x) = T.eval (T.eval prog (p, s)) x
